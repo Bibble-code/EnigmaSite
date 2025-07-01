bf7bdebe4bb235c9cdc2fa7b85dda34e9be5454b
Bibble-code <bibblson@web.de>
2025-07-01 20:05:02 +0200

    added credetials

--- src/views/InstructionsView.vue
+++ src/views/InstructionsView.vue
@@ -1,6 +1,16 @@
 <template>
- Anleitung
-
+  <div class="anleitung">
+    <h1>Projekt im Rahmen der Bachelorarbeit</h1>
+    <p>
+      Diese Webseite wurde im Zuge meiner Bachelorarbeit an der  
+      <strong>Hochschule Ravensburg-Weingarten</strong> entwickelt.<br />
+      Der Quellcode zur Umsetzung der Thesis ist auf GitHub verfügbar:<br />
+      <a href="https://github.com/Bibble-code/Enigma-Zyklometer-Notes" target="_blank" rel="noopener noreferrer">
+        https://github.com/Bibble-code/Enigma-Zyklometer-Notes
+      </a><br />
+      Entwickelt von Tobias Steidle.
+    </p>
+  </div>
 </template>
 
 <script setup>
